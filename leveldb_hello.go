bf2ff684b0bf6b7cb207ec7568703c792526a4be
Don Endres <don@endres.us>
2014-06-03 23:40:30 +0000

    working on terms and tokens in leveldb

--- leveldb_hello.go
+++ leveldb_hello.go
@@ -7,9 +7,7 @@ import (
 )
 
 /*
-add some test terms to the leveldb term database
-
-term -> []byte is null character separated tokens
+add a term to the leveldb term database:
 
 bootstrap assumptions:
 * static time range
@@ -30,15 +28,16 @@ func main() {
 	w := table.NewWriter(f0, nil)
 
 	hello := []byte("hello")
+	world := []byte("the whole world")
 
-	// XXX replace world with null character separated tokens for testing
-
-	world := []byte("world of crap containing the whole world")
 	w.Set(hello, world, nil)
-
 	w.Close() //must call to write to file
 
-	f1, _ := dbfs.Open(dbname)
+	f1, err := dbfs.Open(dbname)
+	if err != nil {
+		log.Fatalln("error getting record", err)
+	}
+
 	r := table.NewReader(f1, nil)
 
 	out, err := r.Get([]byte("hello"), nil)
